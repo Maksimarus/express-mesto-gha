9f34ba56f7805a7ee62e7f3d5c6bf21c8148ab5c
Maksim Chuchuk <cucukmaksim@gmail.com>
2022-11-08 15:36:10 +0300

    feat: реализовать restAPI для проекта mesto

---
+++ controllers/card.js
@@ -0,0 +1,85 @@
+const Card = require('../models/card');
+const { BAD_REQUEST, NOT_FOUND, DEFAULT } = require('../errors/statusCodes');
+
+const getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send(cards))
+    .catch(() => res.status(DEFAULT).send({ message: 'Произошла ошибка' }));
+};
+
+const createCard = (req, res) => {
+  const { name, link } = req.body;
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.status(201).send(card))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST).send({ message: 'Введены некорректные данные карточки пользователя' });
+      } else {
+        res.status(DEFAULT).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+const deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => {
+      if (!card) {
+        res.status(NOT_FOUND).send({ message: 'Карточка с данным id не найдена' });
+        return;
+      }
+      res.send(card);
+    })
+    .catch(() => res.status(DEFAULT).send({ message: 'Произошла ошибка' }));
+};
+
+const likeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, {
+    new: true,
+    runValidators: true,
+  })
+    .then((card) => {
+      if (!card) {
+        res.status(NOT_FOUND).send({ message: 'Карточка с данным id не найдена' });
+        return;
+      }
+      res.send(card);
+    })
+    .catch((err) => {
+      console.log(err.name);
+      if (err.name === 'CastError') {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные для постановки лайка' });
+      } else {
+        res.status(DEFAULT).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+const dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, {
+    new: true,
+    runValidators: true,
+  })
+    .then((card) => {
+      if (!card) {
+        res.status(NOT_FOUND).send({ message: 'Карточка с данным id не найдена' });
+        return;
+      }
+      res.send(card);
+    })
+    .catch((err) => {
+      console.log(err.name);
+      if (err.name === 'CastError') {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные для снятия лайка' });
+      } else {
+        res.status(DEFAULT).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+module.exports = {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+};
